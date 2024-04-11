9d54482c5836b22cc90fb848a2b63c37f5c4c508
Lobsang Sanchez <lobsang.sanchez@polygon.us>
2024-04-11 14:52:26 -0500

    fix: some IDE report an error

--- Runtime/APIManager.cs
+++ Runtime/APIManager.cs
@@ -30,6 +30,8 @@ namespace UnityREST
             {
                 return CoroutineRunner?.Invoke(path(validPath));
             }
+
+            return default;
         }
 
         #region Handlers
